da1d17d114134c38858df692f3b62173e1499601
Rafael <rp032465@gmail.com>
2026-05-18 20:59:42 -0300

    fix: ajustado import do App.js para a pasta screens real

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-//  Caminho corrigido para a sua pasta real de rotas
-import Routes from './src/routes'; 
+// 🧭 Importando a LoginScreen direto da pasta correta que vimos no seu print
+import LoginScreen from './src/screens/LoginScreen'; 
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Routes />
+      <LoginScreen />
     </NavigationContainer>
   );
 }
